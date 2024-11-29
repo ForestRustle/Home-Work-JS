e3a61f19028b5dff8d63e9c34e1ac326ef36fb8e
ForestRustle <116354976+ForestRustle@users.noreply.github.com>
2024-11-30 01:29:33 +0300

    fix - Добавлен вывод в консоль

--- 6-driver/main.js
+++ 6-driver/main.js
@@ -2,5 +2,4 @@ const hasLicense = true;
 const age = 18;
 const isDrunk = true;
 
-const canDrive = age >= 18 && hasLicense && !isDrunk;
-console.log(canDrive);
+const canDrive = age >= 18 && hasLicense && !isDrunk ? console.log('Может'): console.log('Не может');
